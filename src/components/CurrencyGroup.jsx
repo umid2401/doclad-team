d5c3b5b47a6ca3eec6c15ae84e57767aa1d44ed9
umid2401 <umamatraximov@gmail.com>
2025-06-23 13:56:24 +0500

    all

--- src/components/CurrencyGroup.jsx
+++ src/components/CurrencyGroup.jsx
@@ -2,28 +2,21 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const CurrencyGroup = ({ currencies, slideId }) => {
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={`currencies-${slideId}`}
-        // initial={{ opacity: 0 }}
-        // animate={{ opacity: 1 }}
-        // exit={{ opacity: 0 }}
-        // transition={{ duration: 1 }}
-      >
-        {currencies.map((item, i) => (
+    <div key={`currencies-${slideId}`}>
+      {currencies.map((item, i) => (
+        <AnimatePresence mode="wait" key={`currency-wrap-${slideId}-${i}`}>
           <motion.img
-            layout
             key={`currency-${slideId}-${i}-${item.className}`}
             src={item.src}
             className={item.className}
             initial={{ x: 200, rotate: 60, opacity: 0 }}
             animate={{ x: 0, rotate: 0, opacity: 1 }}
             exit={{ x: -200, rotate: -60, opacity: 0 }}
-            transition={{ duration: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
           />
-        ))}
-      </motion.div>
-    </AnimatePresence>
+        </AnimatePresence>
+      ))}
+    </div>
   );
 };
 
--- src/components/SmartMoneySlider.jsx
+++ src/components/SmartMoneySlider.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import "../styles/SmartSlider.css";
-// import CurrencyGroup from "./CurrencyGroup";
+import CurrencyGroup from "./CurrencyGroup";
 
 // Slide matnlari
 const SlideText1 = () => (
@@ -159,29 +159,8 @@ const SmartMoneySlider = () => {
           ))}
         </motion.div>
       </AnimatePresence> */}
-{/* <CurrencyGroup currencies={slide.currencies} slideId={slide.id} /> */}
-<AnimatePresence mode="wait">
-  <motion.div
-    key={`currencies-${prevIndex}-to-${index}`} // <<<< muhim!
-    // initial={{ opacity: 0 }}
-    // animate={{ opacity: 1 }}
-    // exit={{ opacity: 0 }}
-    // transition={{ duration: 1 }}
-  >
-    {slide.currencies.map((item, i) => (
-      <motion.img
-        layout
-        key={`currency-${slide.id}-${i}-${item.className}`}
-        src={item.src}
-        className={item.className}
-        initial={{ x: 200, rotate: 60, opacity: 0 }}
-        animate={{ x: 0, rotate: 0, opacity: 1 }}
-        exit={{ x: -200, rotate: -60, opacity: 0 }}
-        transition={{ duration: 1 }}
-      />
-    ))}
-  </motion.div>
-</AnimatePresence>
+<CurrencyGroup currencies={slide.currencies} slideId={slide.id} />
+
 
       {/* Static Content */}
       <div className="smart">
